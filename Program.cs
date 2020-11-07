540ea71360b36be6640537d836253d6f41512bba
Lorival Cardoso <looricardoso@hotmail.com>
2020-11-07 16:18:51 -0300

    v.1 do exercicio

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using IngredientesExtras.db;
+using System.Linq;
+
+namespace IngredientesExtras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingredientes tipo Extra - Em ordem alfabética");
+            using (var ingredientes = new hamburgueriaContext())
+            {
+                foreach(var ingredExtra in ingredientes.Ingrediente.Where(f => f.TipoIngredienteId == 3).OrderBy(ing => ing.Nome))
+                {
+                    Console.WriteLine($"Código: {ingredExtra.TipoIngredienteId} - Ingrediente: {ingredExtra.Nome}");
+                }
+            }
+        }
+    }
+}
